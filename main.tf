6a4b211d60d3377ef9a9a0270c9ea888b2ff0f69
arindam83 <ahalder.bkp@gmail.com>
2025-02-16 20:14:39 -0500

    cmt

--- main.tf
+++ main.tf
@@ -34,8 +34,10 @@ resource "azurerm_storage_account" "sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+/*
 resource "azurerm_storage_container" "container" {
   name                  = "function-app-container"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
+*/
